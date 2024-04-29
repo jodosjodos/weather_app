4d7a72d16e1dba85e6a3351fee988921266e7290
jodosjodos <jeandedieu2030@gmail.com>
2024-04-29 06:09:21 +0200

    [add] fix input

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class WeatherApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp( 
       theme: ThemeData.dark(
         useMaterial3: true,
       ),
